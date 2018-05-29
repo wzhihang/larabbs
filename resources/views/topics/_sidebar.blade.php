aa5bc67cdd66a88b90bcdb7b3bfa2747f846824e
vagrant <vagrant@homestead>
2018-05-29 02:39:43 +0000

    Topic index page

---
+++ resources/views/topics/_sidebar.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: han
+ * Date: 2018/5/25
+ * Time: 16:13
+ */
---
+++ resources/views/topics/_topic_list.blade.php
@@ -0,0 +1,50 @@
+@if(count($topics))
+    <div class="media-list">
+        @foreach($topics as $topic)
+            <li class="media">
+                <div class="media-left">
+                    <a href="{{ route('users.show',$topic->user_id) }}">
+                        <img class="media-object img-thumbnail" src="{{ $topic->user->avatar }}" alt="{{ $topic->user->name }}" width="52px;" height="52px;">
+                    </a>
+                </div>
+
+                <div class="media-body">
+                    <div class="media-heading">
+                        <a href="{{ route('topics.show',$topic->id) }}" title="{{ $topic->title }}">
+                            {{ $topic->title }}
+                        </a>
+                        <a class="pull-right" href="{{ route('topics.show',[$topic->id]) }}">
+                            <span class="badge"> {{ $topic->reply_count }} </span>
+                        </a>
+                    </div>
+
+                    <div class="media-body meta">
+                        <a href="" title="{{ $topic->category->name }}">
+                            <span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
+                            &nbsp;{{ $topic->category->name }}
+                        </a>
+
+                        <span>&nbsp;  • &nbsp; </span>
+                        <a href="" title="{{ $topic->user->name }}">
+                            <span aria-hidden="true" class="glyphicon glyphicon-user" ></span>
+                            {{ $topic->user->name }}
+                        </a>
+
+                        <span>&nbsp;  • &nbsp; </span>
+                        <span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+                        <span class="timeago" title="最后活跃于">{{ $topic->updated_at->diffForHumans() }}</span>
+                    </div>
+                </div>
+            </li>
+
+            @if ( ! $loop->last)
+                <hr>
+            @endif
+
+        @endforeach
+    </div>
+@else
+    <div class="empty-block">
+        暂无数据 ~_~
+    </div>
+@endif
