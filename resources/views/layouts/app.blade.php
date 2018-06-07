ea9408c339b07e44b95accf4baf98c4d1971ac02
vagrant <vagrant@homestead>
2018-06-07 02:34:49 +0000

    Admin can manage site setting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="description" content="@yield('description',setting('seo_description','larabbs社区'))">
+    <meta name="keyword" content="@yield('keyword', setting('seo_keyword', 'LaraBBS,社区,论坛,开发者论坛'))">
     <meta name="csrf-token" content="{{csrf_token()}}">
     <title>@yield('title','LaraBBS') -Laravel</title>
 
